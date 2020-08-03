ab90ec61b6adad05ba9e7165f333f10b03ae798e
anhduc109 <ducpham1098@gmail.com>
2020-08-03 12:40:26 +0300

    add styles

--- src/pages/check-out.js
+++ src/pages/check-out.js
@@ -13,11 +13,72 @@ const CheckOutPage = () => {
       <div className="check-out-page">
         <div className="container">
           <div className="row">
-            <div className="check-out-information col-md-8 col-sm-12 col-12">
-              <h4>My information</h4>
+            <div className="check-out-information col-md-7 col-sm-12 col-12">
+              <form>
+                <h5>MY INFORMATION</h5>
+                <div className="check-out-section-wrapper">
+                  <div className="row">
+                    <div className="col-md-6 col-sm-12 col-12">
+                      <div class="form-group">
+                        <label for="firstNameInput check-out-label">
+                          Email<span className="input-required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control check-out-input"
+                          id="emailInput"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12 col-12">
+                      <div class="form-group">
+                        <label for="firstNameInput check-out-label">
+                          Phone number<span className="input-required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control check-out-input"
+                          id="phoneNumberInput"
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 col-sm-12 col-12">
+                      <div class="form-group">
+                        <label for="firstNameInput check-out-label">
+                          First name<span className="input-required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control check-out-input"
+                          id="firstNameInput"
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6 col-sm-12 col-12">
+                      <div class="form-group">
+                        <label for="firstNameInput check-out-label">
+                          Last name<span className="input-required">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control check-out-input"
+                          id="lastNameInput"
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h5>SHIPPING ADDRESS</h5>
+              </form>
             </div>
-            <div className="check-out-summary col-md-4 col-sm-12 col-12">
-              <h4>Your order</h4>
+            <div className="check-out-summary col-md-5 col-sm-12 col-12">
+              <h5>YOUR ORDER</h5>
             </div>
           </div>
         </div>
