60b779ad66bb1f4f60578a58027d9961ca92304a
anhduc109 <ducpham1098@gmail.com>
2020-08-10 17:59:44 +0300

    add styles

--- src/pages/sorry.js
+++ src/pages/sorry.js
@@ -10,15 +10,17 @@ const SorryPage = () => {
       <SEO title="Sorry" keywords={[`Citriavn`, `Citria`, `ecommerce`]} />
       <div className="thank-you-page">
         <div className="container">
-          <h1>Sorry!</h1>
-          <h6>
-            There was a problem in our server that we couldn't process your
-            order. Please try again{" "}
-            <span>
-              <AnimateLink path="/check-out" content="here" />
-            </span>
-            .
-          </h6>
+          <h3>Sorry!</h3>
+          <div className="thank-you-content">
+            <h6>
+              There was a problem in our server that we couldn't process your
+              order. Please try again{" "}
+              <span>
+                <AnimateLink path="/check-out" content="here" />
+              </span>
+              .
+            </h6>
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -9,13 +9,15 @@ const ThankYouPage = () => {
       <SEO title="Thank You" keywords={[`Citriavn`, `Citria`, `ecommerce`]} />
       <div className="thank-you-page">
         <div className="container">
-          <h1>THANK YOU!</h1>
-          <h6>
-            We are getting started on your order right away, and you will
-            receive an order confirmation call from our Customer Service. In the
-            mean time, explore the latest fashion collection and get inspired by
-            new trends at Collection
-          </h6>
+          <h3>THANK YOU!</h3>
+          <div className="thank-you-content">
+            <h6>
+              We are getting started on your order right away, and you will
+              receive an order confirmation call from our Customer Service. In
+              the mean time, explore the latest fashion collection and get
+              inspired by new trends at Collection
+            </h6>
+          </div>
         </div>
       </div>
     </Layout>
