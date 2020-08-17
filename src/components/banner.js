1e500266d85c5f701e7038bf09ee1cddd39ac8db
anhduc109 <ducpham1098@gmail.com>
2020-08-17 12:04:54 +0300

    add styles and refactor code

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,16 +1,5 @@
 import React, { Component } from "react"
-import Slider from "react-slick"
 import Img from "gatsby-image"
-
-var settings = {
-  dots: true,
-  speed: 500,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-}
 
 export default class Banner extends Component {
   render() {
@@ -18,22 +7,11 @@ export default class Banner extends Component {
 
     return (
       <div className="slider-section">
-        <Slider {...settings}>
-          {BannerData.map((items, i) => (
-            <div key={i} className="item">
-              <div className="site-Banner">
-                <Img sizes={items.node.image.fluid} />
-                {/* <div className="Banner-details">
-                  <div>
-                    <span className="sub-title">{items.node.subHeading}</span>
-                    <h1>{items.node.title}</h1>
-                    <Link to="/store">Shop Now</Link>
-                  </div>
-                </div> */}
-              </div>
-            </div>
-          ))}
-        </Slider>
+        <div className="site-Banner">
+          {BannerData.length > 0 && (
+            <Img sizes={BannerData[0].node.image.fluid} />
+          )}
+        </div>
       </div>
     )
   }
