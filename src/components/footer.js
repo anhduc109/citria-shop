3b521461d3233831b9adb65bd9025d8a56efb69e
anhduc109 <ducpham1098@gmail.com>
2020-08-17 14:22:00 +0300

    add styles footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,51 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 
+import FacebookLogo from "../images/facebook.svg"
+import InstagramLogo from "../images/instagram.svg"
+import AnimateLink from "./AnimateLink"
+
 class Footer extends Component {
   render() {
     return (
       <footer className="site-footer">
         <div className="footer_inner">
           <div className="container">
-            <div className="footer-widget footer-content">
+            {/* <div className="footer-widget footer-content">
               <section id="nav_menu-8" className="widget widget_nav_menu">
                 <div className="menu-main-container">
                   <ul id="menu-main" className="menu">
                     <li>
-                      <Link to="/about">About</Link>
+                      <AnimateLink path="/store" color="#fff" content="Store" />
                     </li>
                     <li>
-                      <Link to="/store">Store</Link>
-                    </li>
-                    <li>
-                      <Link to="/contact-us">Contact</Link>
+                      <AnimateLink path="/about" color="#fff" content="About" />
                     </li>
                   </ul>
                 </div>
               </section>
-            </div>
+            </div> */}
             <div className="footer-bottom social-right-menu ">
               <div className="site-info">
-                ©2020 Citria. All rights reserved.
+                <div>
+                  <a href="google.com" target="_blank">
+                    <img
+                      className="footer-facebook"
+                      src={FacebookLogo}
+                      alt="Facebook"
+                    />
+                  </a>
+                  <a href="https://www.instagram.com/citriavn/" target="_blank">
+                    <img
+                      className="footer-instagram"
+                      src={InstagramLogo}
+                      alt="Instagram"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
+            <p>©2020 Citriavn. All rights reserved</p>
           </div>
         </div>
       </footer>
