6b079834ef44486638da5ca92f95fc94620dddb5
anhduc109 <ducpham1098@gmail.com>
2020-08-20 13:28:27 +0300

    change img styles

--- src/templates/collectionDetails.js
+++ src/templates/collectionDetails.js
@@ -10,26 +10,28 @@ const CollectionDetails = data => (
       title={data.data.contentfulCollection.title}
       keywords={[`gatsby`, `ecommerce`, `react`, `contentFul`, `Snipcart`]}
     />
-    {/* <div className="post-thumbnail">
-        <Img sizes={data.data.contentfulCollection.featureImage.fluid} />
-      </div> */}
-    <div className="container">
-      <div className="collection-detail-page">
+    <div className="collection-detail-page">
+      <div className="container">
         <h3>{data.data.contentfulCollection.title}</h3>
-        <div
+        {/* <div
           className="collection-detail-description"
           dangerouslySetInnerHTML={{
             __html:
               data.data.contentfulCollection.description.childMarkdownRemark
                 .html,
           }}
-        ></div>
+        ></div> */}
+      </div>
+      <div className="row">
         <Img
-          className="collection-detail-image"
+          className="collection-detail-image col-12 col-sm-6 col-md-6"
           sizes={data.data.contentfulCollection.featureImage.fluid}
         />
         {data.data.contentfulCollection.moreImages.map(item => (
-          <Img className="collection-detail-image" sizes={item.fluid} />
+          <Img
+            className="collection-detail-image col-12 col-sm-6 col-md-6"
+            sizes={item.fluid}
+          />
         ))}
       </div>
     </div>
